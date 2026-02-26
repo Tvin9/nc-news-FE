ee43e27f081618d7fc0c5a3df533601e16d105fc
Tvin9 <christopherharrison0606@gmail.com>
2026-02-26 12:09:48 +0000

    add Users component and avatar svgs for later use

---
+++ src/components/Users.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+export function Users() {
+	const [users, setUsers] = useState(null);
+	useEffect(() => {
+		async function fetchUsers() {
+			const response = await fetch(
+				'https://nc-news-yyic.onrender.com/api/users/',
+			);
+			const body = await response.json();
+			setUsers(body);
+		}
+		fetchUsers();
+	}, []);
+
+	if (!users) return <p>Loading...</p>;
+
+	return (
+		<main>
+			<ul>
+				{users.users.map((user) => (
+					<li key={user.username}>
+						<p>Username: {user.username}</p>
+						<p>Name: {user.name}</p>
+						<img src={user.avatar_url} alt={user.username} width={50} />
+					</li>
+				))}
+			</ul>
+		</main>
+	);
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { ArticleCard } from './components/ArticleCard.jsx';
+import { Users } from './components/Users.jsx';
 
 createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
 		<Routes>
 			<Route path="/" element={<App />} />
 			<Route path="/articles/:article_id" element={<ArticleCard />} />
+			<Route path="/authors" element={<Users />} />
 		</Routes>
 	</BrowserRouter>,
 );
